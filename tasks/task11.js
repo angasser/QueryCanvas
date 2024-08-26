08d5bcb366d780ea6e871c93ffd357f65f7989fb
sverrirth <sverrir@gmx.com>
2024-08-26 11:26:38 +0200

    improved picnic task

--- tasks/task11.js
+++ tasks/task11.js
@@ -2,17 +2,18 @@ import { createTask } from "../stateManager.js";
 
 const code =
 `
-def go_to_picnic(is_sunny, is_holiday, friends_available, transport_available, park_open):
+def go_to_picnic(is_hot, is_holiday, friends_available, is_rainy, is_concert):
     # Edit this function
     [Q1v]return [Q1]
 `;
 
 const description = 
-`You want to go to a picnic only if any of the following conditions is true:
+`You will go to the park if any of the following conditions is true:
 
-(1) It is sunny and a holiday, and your friends are available.
-(2) It is sunny, and the park is open, and you have transportation, even if it's not a holiday.
-(3) It is a holiday, your friends are available, and you have transportation, even if it's not sunny.`;
+(1) it's hot, your friends are available and it's a holiday,
+(2) there's a concert, your friends are available and it's a holiday,
+(3) it's neither hot nor rainy.`;
+
 
 const queryDescription =
     `Only solve this task with the visual query system.`;
@@ -26,7 +27,7 @@ export function task11(state) {
         ["Q1", {
             varString: "",
             expressionString: "True",
-            expression: `{"id":45740491,"loc":{"expId":45740491,"min":40,"max":44},"varLoc":{"expId":45740491,"min":13,"max":13},"codeVariableOverride":"","codeQueryOverride":"True","queries":{"_type":"Map","entries":[[0,{"id":0,"content":"Main View","color":"#264653"}],[1,{"id":1,"content":"is_sunny","color":"#2A9D8F"}],[2,{"id":2,"content":"is_holiday","color":"#E76F51"}],[3,{"id":3,"content":"friends_available","color":"#457B9D"}],[4,{"id":4,"content":"transport_available","color":"#F4A261"}],[5,{"id":5,"content":"park_open","color":"#E63946"}]]},"activeView":{"id":0,"name":"Main View","trans":{"x":0,"y":0},"scale":1,"shapes":{"_type":"Map","entries":[]},"fragments":{"_type":"Map","entries":[]},"allInactiveFragments":{"_type":"Set","values":[]}},"viewportStates":{"_type":"Map","entries":[[0,{"id":0,"name":"Main View","trans":{"x":0,"y":0},"scale":1,"shapes":{"_type":"Map","entries":[]},"fragments":{"_type":"Map","entries":[]},"allInactiveFragments":{"_type":"Set","values":[]}}]]},"hoveringType":1,"hoveredQueries":{"_type":"Set","values":[]},"hoveredShapes":{"_type":"Set","values":[]},"hoveredFragments":{"_type":"Set","values":[0]},"selectedShapes":{"_type":"Set","values":[]},"boxSelectedShapes":{"_type":"Set","values":[]},"selectedQuery":null,"boxSelectionBox":null,"isBoxSelecting":false,"areQueriesVisible":true,"isViewportSelectionVisible":false,"visibleQueryShapeRows":{"_type":"Set","values":[5]}}`
+            expression: `{"id":45740491,"loc":{"expId":45740491,"min":40,"max":44},"varLoc":{"expId":45740491,"min":13,"max":13},"codeVariableOverride":"","codeQueryOverride":"True","queries":{"_type":"Map","entries":[[0,{"id":0,"content":"Main View","color":"#264653"}],[1,{"id":1,"content":"is_hot","color":"#2A9D8F"}],[2,{"id":2,"content":"is_holiday","color":"#E76F51"}],[3,{"id":3,"content":"friends_available","color":"#457B9D"}],[4,{"id":4,"content":"is_rainy","color":"#F4A261"}],[5,{"id":5,"content":"is_concert","color":"#E63946"}]]},"activeView":{"id":0,"name":"Main View","trans":{"x":0,"y":0},"scale":1,"shapes":{"_type":"Map","entries":[]},"fragments":{"_type":"Map","entries":[]},"allInactiveFragments":{"_type":"Set","values":[]}},"viewportStates":{"_type":"Map","entries":[[0,{"id":0,"name":"Main View","trans":{"x":0,"y":0},"scale":1,"shapes":{"_type":"Map","entries":[]},"fragments":{"_type":"Map","entries":[]},"allInactiveFragments":{"_type":"Set","values":[]}}]]},"hoveringType":1,"hoveredQueries":{"_type":"Set","values":[]},"hoveredShapes":{"_type":"Set","values":[]},"hoveredFragments":{"_type":"Set","values":[0]},"selectedShapes":{"_type":"Set","values":[]},"boxSelectedShapes":{"_type":"Set","values":[]},"selectedQuery":null,"boxSelectionBox":null,"isBoxSelecting":false,"areQueriesVisible":true,"isViewportSelectionVisible":false,"visibleQueryShapeRows":{"_type":"Set","values":[5]}}`
         } ],
     ]));
 
